b5f7210f70c880145c5ab0d8fe7879fd586327d9
blackeye75 <priyanshuraj22275@gmail.com>
2026-04-15 23:29:06 +0530

    chnages

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 console.log( process.env.CLIENT_URL)
 app.use(
   cors({
-    origin:"https://gymwebsite-1-ft70.onrender.com/",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true
   })
 );
